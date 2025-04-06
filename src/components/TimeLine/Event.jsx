5859234b27c2effb822127d7a713c92a11bd523d
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-04-05 23:47:50 -0300

    Fix event lines (#5)
    * fix timeline function
    
    * add gaps
    
    * improve tooltip to see start and end date

--- src/components/TimeLine/Event.jsx
+++ src/components/TimeLine/Event.jsx
@@ -2,8 +2,11 @@ import "./timeline.css";
 import { Tooltip } from "../sharable/ToolTip";
 
 export const Event = ({ event, totalDuration }) => {
-  const { name, duration } = event;
+  const { name, duration, isEvent, start, end } = event;
   const percentWidth = (duration / totalDuration) * 100;
+
+  if (!isEvent)
+    return <div style={{ width: `${percentWidth}%` }} className="gap-event" />;
 
   return (
     <Tooltip
@@ -12,7 +15,15 @@ export const Event = ({ event, totalDuration }) => {
           {name}
         </div>
       }
-      content={name}
+      content={
+        <div className="event-tooltip-content">
+          {name}
+          <ul>
+            <li>Start: {start}</li>
+            <li>End: {end}</li>
+          </ul>
+        </div>
+      }
     />
   );
 };
