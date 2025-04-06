2acf45669420cdd48f940c62da5517187aad2d12
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-04-05 23:41:06 -0300

    add gaps

--- src/components/TimeLine/Event.jsx
+++ src/components/TimeLine/Event.jsx
@@ -2,8 +2,11 @@ import "./timeline.css";
 import { Tooltip } from "../sharable/ToolTip";
 
 export const Event = ({ event, totalDuration }) => {
-  const { name, duration } = event;
+  const { name, duration, isEvent } = event;
   const percentWidth = (duration / totalDuration) * 100;
+
+  if (!isEvent)
+    return <div style={{ width: `${percentWidth}%` }} className="gap-event" />;
 
   return (
     <Tooltip
