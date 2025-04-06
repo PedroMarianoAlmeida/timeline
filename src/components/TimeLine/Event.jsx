6067e0a6cb4b34b19cce3570e4a4e09211ed96c4
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-04-05 23:47:09 -0300

    improve tooltip to see start and end date

--- src/components/TimeLine/Event.jsx
+++ src/components/TimeLine/Event.jsx
@@ -2,7 +2,7 @@ import "./timeline.css";
 import { Tooltip } from "../sharable/ToolTip";
 
 export const Event = ({ event, totalDuration }) => {
-  const { name, duration, isEvent } = event;
+  const { name, duration, isEvent, start, end } = event;
   const percentWidth = (duration / totalDuration) * 100;
 
   if (!isEvent)
@@ -15,7 +15,15 @@ export const Event = ({ event, totalDuration }) => {
           {name}
         </div>
       }
-      content={name}
+      content={
+        <div className="event-tooltip-content">
+          {name}
+          <ul>
+            <li>Start: {start}</li>
+            <li>End: {end}</li>
+          </ul>
+        </div>
+      }
     />
   );
 };
